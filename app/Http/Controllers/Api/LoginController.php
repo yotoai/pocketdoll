32fb101e8cb4088e90a0ddca8bf9fe58eb35e672
yotoai <473203432@qq.com>
2017-11-30 18:59:33 +0800

    'ratiozhengzaijinxing'

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -19,7 +19,6 @@ class LoginController extends BaseController
     // 用户授权 (跳转登录)
     public function login(Request $request)
     {
-	\Log::info($request->all());
         $this->validate($request,[
             'sdkId'  => 'required',
             'userId' => 'required',
