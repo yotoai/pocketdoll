7ab4c117247892096d9f9d13110141d8b7c33b75
yotoai <473203432@qq.com>
2017-12-15 10:34:24 +0800

    '1215'

--- app/Model/DollMachineLog.php
+++ app/Model/DollMachineLog.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class DollMachineLog extends Model
 {
-    //
+    // 指定表
+    protected $table = 'doll_machine_log';
+
+    // 指定字段
+    protected $fillable = ['sdk_id','doll_machine_id','doll_machine_name','catch_num','catched_num','lucky_model_catch_num'];
 }
