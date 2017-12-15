0297e55ae1873cd023ed70b52d6087cdf3e70562
yotoai <473203432@qq.com>
2017-12-15 13:09:09 +0800

    '1215'

--- app/Console/Commands/clearRedis.php
+++ app/Console/Commands/clearRedis.php
@@ -50,6 +50,7 @@ class clearRedis extends Command
                 Redis::del($v->user_id . '_charge');
                 Redis::del($v->user_id . '_mission');
                 Redis::del($v->user_id . '_shareWithWx');
+                Redis::del($v->user_id . '_isdraw');
                 $keys = Redis::keys($v->user_id . '_*_mission');
                 foreach ($keys as $s){
                     Redis::del($s);
